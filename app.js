3f72116d34a254eb72bd1b2fe0033ce73829d64a
wasifashraf01 <mw431019@gmail.com>
2025-09-29 17:10:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-function getNumber(num){
-    var result = document.getElementById("result").value;
-    result.value += num; 
-    console.log(result);
+function getNumber(n){
+    var result = document.getElementById("myInput");
+    result.value += n; 
 }
 
+
 function clearAll() {
-    
+    var clear = document.getElementById("myInput");
+    clear.value = "";
+}
+
+function del(){
+    var delNum = document.getElementById("myInput");
+    delNum.value = delNum.value.slice(0, -1);
+}
+
+function calculate(){
+    var ans = document.getElementById("myInput");
+    ans.value = eval(ans.value);
 }
